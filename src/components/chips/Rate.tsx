c4d2d101bf55ce3bd7a27f59e379bfe69c3f1581
ivanmirable <ivankrivonogov1823@mail.ru>
2024-02-17 19:59:29 +0400

    feat: fix rate

--- src/components/chips/Rate.tsx
+++ src/components/chips/Rate.tsx
@@ -6,11 +6,12 @@ import { StarIcon } from 'src/shared';
 const Container = styled.div`
   display: flex;
 `;
-const StarInput = styled.input`
+
+const Star = styled.input`
   display: none;
 `;
 
-interface RateProps extends InputHTMLAttributes<HTMLInputElement> {
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
   Icon?: JSX.Element;
   disabled?: boolean;
   title?: string;
@@ -18,41 +19,36 @@ interface RateProps extends InputHTMLAttributes<HTMLInputElement> {
   currentRating?: number;
 }
 
-const StartRate = (props: RateProps): ReactElement => {
+const StartRate = (props: Props): ReactElement => {
   const { ...rest } = props;
-  const [hover, setHover] = useState<number>(0);
-  const [rating, setRating] = useState<number>(0);
+  const [hover, setHover] = useState(0);
+  const [rating, setRating] = useState(0);
 
   const handleMouseOver = (index: number) => {
-    setHover(index + 1);
+    setHover(index);
   };
 
   const handleClick = (index: number) => {
-    setRating(index + 1);
+    setRating(index);
   };
 
   return (
     <Container>
-      {[...Array(5)].map((star, index) => {
-        const currentrating = index + 1;
-
-        return (
-          // eslint-disable-next-line react/jsx-key
-          <label>
-            <StarInput
-              type="radio"
-              value={currentrating}
-              onClick={() => handleClick(index)}
-              {...rest}
-            />
-            <StarIcon
-              color={(hover || rating) >= currentrating ? 'orange' : 'gray'}
-              key={index}
-              onMouseOver={() => handleMouseOver(index)}
-            />
-          </label>
-        );
-      })}
+      {[...Array(5)].map((star, index) => (
+        <label key={index}>
+          <Star
+            type="radio"
+            value={index}
+            onClick={() => handleClick(index)}
+            {...rest}
+          />
+          <StarIcon
+            color={(hover || rating) >= index ? 'orange' : 'gray'}
+            key={index}
+            onMouseOver={() => handleMouseOver(index)}
+          />
+        </label>
+      ))}
     </Container>
   );
 };
