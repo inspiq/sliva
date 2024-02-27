9060799f15c1d96a404a6400197d365eaab99ae5
ivanmirable <ivankrivonogov1823@mail.ru>
2024-02-27 17:51:30 +0400

    feat: demo review

--- src/modules/specialists/specialist_profile/specialist_reviews/ReviewForm.tsx
+++ src/modules/specialists/specialist_profile/specialist_reviews/ReviewForm.tsx
@@ -1,10 +1,4 @@
-import {
-  FormEvent,
-  ReactElement,
-  TextareaHTMLAttributes,
-  useMemo,
-  useState,
-} from 'react';
+import { ReactElement, TextareaHTMLAttributes, useMemo, useState } from 'react';
 import { collection, doc, setDoc, updateDoc } from 'firebase/firestore';
 import styled from 'styled-components';
 import { v4 as uuidv4 } from 'uuid';
